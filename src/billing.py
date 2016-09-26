cba360500b113f2a3e0ad8788888e06165691002
Emanahan <l1ghtz3r@gmail.com>
2016-09-26 14:06:43 -0400

    Demo-able with test user

--- src/billing.py
+++ src/billing.py
@@ -1,19 +1,64 @@
-from flask import Flask, render_template, request, g
-from flask_login import login_required
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import login_required, LoginManager, login_user, logout_user, current_user
 from dateutil.parser import parse
 from collaboratory import Collaboratory
+from user_management import User, UserDatabase
 
 app = Flask(__name__)
 
+app.secret_key = "random, secret, super duper secret key"
+
 database = Collaboratory.default_init()
+
+users = UserDatabase()
+users.init_db()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = '/login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.get_test_user(user_id)
 
 
 @app.route('/')
+@login_required
 def root():
     return render_template('template.html', projects=database.get_projects())
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print "attempting login"
+    if request.method == "GET":
+        return render_template("login.html")
+    elif request.method == "POST":
+        user = users.get_test_user(7)
+        if request.form.get('username') == user.username and request.form.get('password') == user.password:
+            login_user(user)
+            flash("LOGIN SUCCESSFUL")
+            print "login successful"
+            return redirect(url_for('root'))
+        else:
+            flash("LOGIN FAILED", 'error')
+            return render_template("login.html",
+                                   username=request.form.get('username'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    user = current_user
+    user.authenticated = False
+    logout_user()
+    flash("LOGGED OUT")
+    return redirect('login')
+
+
 @app.route('/by_user', methods=['GET'])
+@login_required
 def calculate_cost_by_user():
     start_date = parse(request.args.get('start_date'), ignoretz=True)
     end_date = parse(request.args.get('end_date'), ignoretz=True)
@@ -38,7 +83,9 @@ def calculate_cost_by_user():
 
 
 # Might wanna do javascript in order to allow for a dynamic path variable
+# Need to add auth so that a user can't manually enter project details and access projects they shouldn't
 @app.route('/by_project', methods=['GET'])
+@login_required
 def calculate_cost_by_project():
     start_date = parse(request.args.get('start_date'), ignoretz=True)
     end_date = parse(request.args.get('end_date'), ignoretz=True)
@@ -50,7 +97,7 @@ def calculate_cost_by_project():
 
     image_gigabyte_hours = database.get_image_storage_gigabyte_hours_by_project(start_date, end_date, project_id)
 
-    users = database.get_users_by_project(project_id)
+    project_users = database.get_users_by_project(project_id)
 
     return render_template('search_by_project.html',
                            start_date=start_date,
@@ -59,7 +106,7 @@ def calculate_cost_by_project():
                            volume_gb_hours=volume_gigabyte_hours,
                            image_gb_hours=image_gigabyte_hours,
                            project_id=project_id,
-                           users=users,
+                           users=project_users,
                            projects=database.get_projects())
 
 
--- src/user_management.py
+++ src/user_management.py
@@ -1,1 +1,95 @@
-from flask_login import LoginManager, UserMixin
+import records
+
+
+class User:
+
+    def __init__(self, user_id, username, password, projects):
+        self.username = username
+        self.password = password
+        self.user_id = user_id
+        self.anonymous = False
+        self.active = True
+        self.authenticated = False
+        self.projects = projects
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return self.anonymous
+
+    def get_id(self):
+        return self.user_id
+
+
+class UserDatabase:
+
+    def __init__(self):
+        self.database = records.Database('sqlite:///billing.db')
+        self.init_db()
+
+    def init_db(self):
+        self.database.query(
+            '''
+            CREATE TABLE IF NOT EXISTS users (
+                id  INTEGER PRIMARY KEY AUTOINCREMENT,
+                username       TEXT     NOT NULL,
+                password       TEXT     NOT NULL,
+                projects       BLOB);
+            ''')
+
+    def get_test_user(self, user_id):
+        return User(7, "admin", "test", [])
+
+    def get_user_by_id(self, user_id):
+        user_data = self.database.query(
+            '''
+            SELECT *
+
+            FROM
+              users
+
+            WHERE
+              id = :user_id;
+            ''',
+            user_id=user_id)
+        user = user_data[0]
+        return User(user["id"], user["username"], user["password"], user["projects"].split(","))
+
+    def update_user(self, user):
+        self.database.query(
+            '''
+            UPDATE
+              users
+
+            SET
+              username = :username,
+              password = :password,
+              projects = :projects
+
+            WHERE
+              id = :user_id;
+            ''',
+            user_id=user.user_id,
+            username=user.username,
+            password=user.password,
+            projects=','.join(user.projects))
+
+    def create_user(self, username, password, projects):
+        user_id = self.database.query(
+            '''
+            INSERT INTO
+              users
+
+              (username, password, projects)
+
+            VALUES
+              (:username, :password, :projects);
+            ''',
+            username=username,
+            password=password,
+            projects=','.join(projects))
+        return self.get_user_by_id(user_id)
