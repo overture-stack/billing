79a1056fb4e4e74e67214225164635bdaf2f406c
Emanahan <l1ghtz3r@gmail.com>
2016-10-07 16:23:30 -0400

    Login and token logic

--- billing-api/billing/__init__.py
+++ billing-api/billing/__init__.py
@@ -1,87 +1,66 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_login import login_required, LoginManager, login_user, logout_user, current_user
+from flask import Flask, render_template, request, flash, redirect, Response
 from dateutil.parser import parse
 from collaboratory import Collaboratory
-from user_management import UserDatabase
 from auth import sessions
 from config import default
+import json
+from error import APIError, AuthenticationError
+from functools import wraps
 
 app = Flask(__name__)
 app.config.from_object(default)
 
 app.secret_key = app.config['SECRET_KEY']
 
-# TODO: Remove, this was for proof of concept
-token = sessions.get_new_token(auth_url=app.config['AUTH_URI'], username='admin', password='admin')
 database = Collaboratory(app.config['MYSQL_URI'])
 
-users = UserDatabase()
-users.init_db()
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = '/login'
+def authenticate(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        print 'Authorizing'
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            token = auth_header.split()[1]
+            c = sessions.validate_token(app.config['AUTH_URI'], token)
+            retval = func(c, *args, **kwargs)
+            retjson = json.loads(retval.data)
+            retjson['token'] = sessions.renew_token(app.config['AUTH_URI'], token)
+            retval.data = json.dumps(retjson)
+            return retval
+        else:
+            raise AuthenticationError("Authentication required: Token not provided")
+    return inner
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    if user_id == 7:
-        return users.get_test_user(7)
-    else:
-        return users.get_user_by_id(user_id)
-
-
-def get_relevant_projects():
-    if current_user.username == 'admin':
-        return database.get_projects()
-    else:
-        return current_user.projects
+@app.errorhandler(APIError)
+def api_error_handler(e):
+    return Response(e.response_body, status=e.code, content_type='application/json')
 
 
 @app.route('/')
-@login_required
-def root():
-    return render_template('template.html',
-                           projects=get_relevant_projects())
+@authenticate
+def root(client):
+    return Response('{"MY_DATA":"SOMETHING"}', 200)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    print 'attempting login'
-    if request.method == 'GET':
-        return render_template('login.html')
-    elif request.method == 'POST':
-
-        if request.form.get('username') == 'admin':
-            user = users.get_test_user(7)
-        else:
-            user = users.get_user_by_username(request.form.get('username'))
-
-        if user is not None and user.authenticate(request.form.get('username'),
-                                                  request.form.get('password')):
-            login_user(user)
-            flash('LOGIN SUCCESSFUL')
-            print current_user.username
-            print current_user.projects
-            return redirect(url_for('root'))
-        else:
-            flash('LOGIN FAILED', 'error')
-            return render_template('login.html',
-                                   username=request.form.get('username'))
+    token = sessions.get_new_token(
+        auth_url=app.config['AUTH_URI'],
+        username=request.json['username'],
+        password=request.json['password'])
+    response_json = json.dumps({'token': token})
+    return Response(response_json, status=200, content_type='application/json')
 
 
-@app.route('/logout')
-@login_required
-def logout():
-    user = current_user
-    user.authenticated = False
-    logout_user()
-    flash('LOGGED OUT')
-    return redirect('login')
+@app.route('/users', methods=['GET'])
+@authenticate
+def get_users(client):
+    return None
 
 
 @app.route('/by_user', methods=['GET'])
-@login_required
 def calculate_cost_by_user():
     start_date = parse(request.args.get('start_date'), ignoretz=True)
     end_date = parse(request.args.get('end_date'), ignoretz=True)
@@ -94,21 +73,12 @@ def calculate_cost_by_user():
 
     project_users = database.get_users_by_project(project_id)
 
-    return render_template('search_by_user.html',
-                           start_date=start_date,
-                           end_date=end_date,
-                           instance_core_hours=instance_core_hours,
-                           volume_gb_hours=volume_gigabyte_hours,
-                           project_id=project_id,
-                           users=project_users,
-                           current_user_id=user_id,
-                           projects=get_relevant_projects())
+    return None
 
 
 # Might wanna do javascript in order to allow for a dynamic path variable
 # Need to add auth so that a user can't manually enter project details and access projects they shouldn't
 @app.route('/by_project', methods=['GET'])
-@login_required
 def calculate_cost_by_project():
     start_date = parse(request.args.get('start_date'), ignoretz=True)
     end_date = parse(request.args.get('end_date'), ignoretz=True)
@@ -122,12 +92,4 @@ def calculate_cost_by_project():
 
     project_users = database.get_users_by_project(project_id)
 
-    return render_template('search_by_project.html',
-                           start_date=start_date,
-                           end_date=end_date,
-                           instance_core_hours=instance_core_hours,
-                           volume_gb_hours=volume_gigabyte_hours,
-                           image_gb_hours=image_gigabyte_hours,
-                           project_id=project_id,
-                           users=project_users,
-                           projects=get_relevant_projects())
+    return None
--- billing-api/billing/auth/sessions.py
+++ billing-api/billing/auth/sessions.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from keystoneclient.v2_0 import client
+from keystoneclient.exceptions import Unauthorized
+from ..error import AuthenticationError, APIError
 
 
 def get_new_token(auth_url=None, username=None, password=None):
@@ -15,10 +17,46 @@ def get_new_token(auth_url=None, username=None, password=None):
 
     response = requests.post(auth_url+'/tokens', json=request_json)
     response_json = json.loads(response.content)
-    token = response_json['access']['token']['id']
-    c = client.Client(auth_url=auth_url, token=token)
-    print c
+    if response.status_code == 401:
+        raise AuthenticationError('Invalid user/password')
+    elif response.status_code == 200:
+        token = response_json['access']['token']['id']
+        return token
+    else:
+        raise APIError(response.status_code, response_json['error']['title'], response_json['error']['message'])
+
+
+def renew_token(auth_url=None, token=None):
+    request_json = {
+        'auth': {
+            'token': {
+                'id': token
+            }
+        }
+    }
+
+    response = requests.post(auth_url + '/tokens', json=request_json)
+    response_json = json.loads(response.content)
+    if response.status_code == 401:
+        raise AuthenticationError('Token expired. Please login again.')
+    elif response.status_code == 200:
+        token = response_json['access']['token']['id']
+        return token
+    else:
+        raise APIError(response.status_code, response_json['error']['title'], response_json['error']['message'])
+
+
+# Returns a client
+def validate_token(auth_url=None, token=None):
+    try:
+        c = client.Client(auth_url=auth_url, token=token)
+        return c
+    except Unauthorized:
+        # Take their error and resend it as mine
+        raise AuthenticationError('Authentication required: Invalid token')
 
 
 def list_projects(auth_url=None, token=None):
-    return client.Client(auth_url=auth_url, token=token).tenants.list()
+    c = client.Client(auth_url=auth_url, token=token)
+    return c.tenants.list()
+
