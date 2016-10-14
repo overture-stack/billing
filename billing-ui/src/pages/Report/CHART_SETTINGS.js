f30d016b2ca624c2ae2a6652f324757263dca496
Chang Wang <garnwraly@gmail.com>
2016-10-14 11:51:14 -0400

    minor: fix spacing

--- billing-ui/src/pages/Report/CHART_SETTINGS.js
+++ billing-ui/src/pages/Report/CHART_SETTINGS.js
@@ -2,51 +2,51 @@ import {Highcharts} from 'react-highcharts';
 import {getSeriesFromReportEntries} from './getSeriesFromReportEntries';
 
 export default {
-    colors: [
-        '#3498DB',
-        '#E74C3C',
-        '#2C3E50',
-    ],
-        chart: {
-            type: 'area'
-        },
-        title: {
-            text: ''
-        },
-        xAxis: {
-            type: 'datetime',
-            title: {
-                enabled: false
-            },
-            dateTimeLabelFormats: {
-                hour: '%l:%M %p'
-            },
-            labels: {
-              formatter: function() {
-                console.log(this.value)
-                return Highcharts.dateFormat('%a %d %b', this.value);
-              }
-            },
-        },
-        yAxis: {
-            title: {
-                text: 'Usage'
-            },
-        },
-        tooltip: {
-            split: true,
-            // valueSuffix: ' millions'
-        },
-        plotOptions: {
-            area: {
-                stacking: 'normal',
-                lineColor: '#666666',
-                lineWidth: 0,
-                marker: {
-                    lineWidth: 0,
-                    lineColor: '#666666'
-                }
-            }
-        },
-        series: getSeriesFromReportEntries([])
-    };
+  colors: [
+    '#3498DB',
+    '#E74C3C',
+    '#2C3E50',
+  ],
+  chart: {
+    type: 'area'
+  },
+  title: {
+    text: ''
+  },
+  xAxis: {
+    type: 'datetime',
+    title: {
+      enabled: false
+    },
+    dateTimeLabelFormats: {
+      hour: '%l:%M %p'
+    },
+    labels: {
+      formatter: function () {
+        console.log(this.value)
+        return Highcharts.dateFormat('%a %d %b', this.value);
+      }
+    },
+  },
+  yAxis: {
+    title: {
+      text: 'Usage'
+    },
+  },
+  tooltip: {
+    split: true,
+    // valueSuffix: ' millions'
+  },
+  plotOptions: {
+    area: {
+      stacking: 'normal',
+      lineColor: '#666666',
+      lineWidth: 0,
+      marker: {
+        lineWidth: 0,
+        lineColor: '#666666'
+      }
+    }
+  },
+  series: getSeriesFromReportEntries([])
+};
