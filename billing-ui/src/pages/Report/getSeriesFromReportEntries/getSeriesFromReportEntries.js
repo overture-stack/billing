4edf6f034b3113ffbbbc7daf57e67d93d61422ce
Chang Wang <garnwraly@gmail.com>
2016-10-14 11:18:37 -0400

    aggregate buckets based on fromDate

--- billing-ui/src/pages/Report/getSeriesFromReportEntries/getSeriesFromReportEntries.js
+++ billing-ui/src/pages/Report/getSeriesFromReportEntries/getSeriesFromReportEntries.js
@@ -1,12 +1,24 @@
 import moment from 'moment';
+import _ from 'lodash';
 
 export function getSeriesFromReportEntries(entries) {
+  const aggregatedEntries = _(entries)
+    .groupBy('fromDate')
+    .values()
+    .map(items => items.reduce((acc = {cpu: 0, volume: 0, image: 0}, a) => ({
+      ...acc,
+      ...a,
+      cpu: acc.cpu + (a.cpu || 0),
+      volume: acc.volume + (a.volume || 0),
+      image: acc.image + (a.image || 0),
+    })))
+    .value();
   return ['cpu', 'image', 'volume']
     .map(metric => ({
       name: metric,
-      data: entries.map(entry => ({
-        x: moment(entry.fromDate, 'YYYY-MM-DD').toDate(),
-        y: entry[metric],
+      data: aggregatedEntries.map(entry => ({
+        x: moment(entry.fromDate, moment.ISO_8601).unix(),
+        y: entry[metric] || 0,
       }))
     }))
 }
