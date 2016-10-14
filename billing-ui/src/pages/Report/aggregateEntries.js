24fc28d9d33522f06f29e4f04559ead7d1012ffa
Chang Wang <garnwraly@gmail.com>
2016-10-14 16:57:45 -0400

    include groupBy’ed key in aggregateEntries items

--- billing-ui/src/pages/Report/aggregateEntries.js
+++ billing-ui/src/pages/Report/aggregateEntries.js
@@ -1,15 +1,16 @@
 import _ from 'lodash';
 
 export function aggregateEntries(entries, groupByIteratee) {
+  console.log(_.groupBy(entries, groupByIteratee));
   return _(entries)
     .groupBy(groupByIteratee)
-    .values()
-    .map(items => items.reduce((acc, entry) => ({
+    .map((items, key) => items.reduce((acc, entry) => ({
       ...acc,
       ...entry,
       cpu: (acc.cpu || 0) + (entry.cpu || 0),
       volume: (acc.volume || 0) + (entry.volume || 0),
       image: (acc.image || 0) + (entry.image || 0),
+      key,
     })))
     .value();
 }
