84b74b6981350c3f6de82d111dd7c9fe2778156c
andricDu <dusan.andric@oicr.on.ca>
2016-10-20 12:13:31 -0400

    Adds comments to config

--- billing-api/billing/config/default.py
+++ billing-api/billing/config/default.py
@@ -1,5 +1,5 @@
-DEBUG = True
+DEBUG = True  # Debug mode for flask
 SECRET_KEY = 'random, secret, super duper secret key'
-AUTH_URI = 'http://142.1.177.124:5000/v2.0'
-MYSQL_URI = 'mysql://root:test@142.1.177.124:3306'
-VALID_BUCKET_SIZES = ['daily', 'weekly', 'monthly', 'yearly']
+AUTH_URI = 'http://142.1.177.124:5000/v2.0'  # Keystone/Identity API endpoint
+MYSQL_URI = 'mysql://root:test@142.1.177.124:3306'  # Mysql URI
+VALID_BUCKET_SIZES = ['daily', 'weekly', 'monthly', 'yearly']  # Bucketing options for query.
